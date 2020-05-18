2879b64cc14e07f0fb2f84fa13cd85f3e9eadd5f
Dima Timoshenko <tzd04094@gmail.com>
2020-05-18 16:05:36 +0300

    Set up next js

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,29 +1,12 @@
-import express from 'express';
-import 'express-async-errors';
-import {json} from 'body-parser';
-
-import {currentUserRouter} from './routes/current-user';
-import {signinRouter} from './routes/signin';
-import {signupRouter} from './routes/signup';
-import {signoutRouter} from './routes/signout';
-import {errorHandler} from './middlewares/error-handler';
-import {NotFoundError} from './errors/not-found-error';
 import mongoose from 'mongoose';
 
-const app = express();
-app.use(json());
+import {app} from './app'
 
-app.use(currentUserRouter);
-app.use(signinRouter);
-app.use(signupRouter);
-app.use(signoutRouter);
-
-app.all('*', async (req, res, next) => {
-	throw new NotFoundError();
-});
-app.use(errorHandler);
 
 const start = async () => {
+	if (!process.env.JWT_KEY) {
+		throw new Error('JWT_KEY must be definded');
+	}
 	try {
 		await mongoose.connect('mongodb://auth-mongo-srv', {
 			useNewUrlParser: true,
--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,10 +1,54 @@
-import express from 'express';
+import express, {Request, Response} from 'express';
+import {body} from 'express-validator';
+import jwt from 'jsonwebtoken';
+
+import {User} from '../models/user';
+import {validateRequest} from '../middlewares/validate-request';
+import {BadRequestError} from '../errors/bad-request-error';
+import {Password} from '../services/password';
 
 const router = express.Router();
 
-router.post('/api/users/signin', (req, res) => {
-	res.send('Hi there!');
-});
+router.post('/api/users/signin',
+	[
+		body('email')
+			.isEmail()
+			.withMessage('Email must be valid'),
+	  body('password')	
+			.trim()
+			.notEmpty()
+			.withMessage('You must supply a password')
+	],
+	validateRequest,
+	async (req: Request, res:Response) => {
+    const {email, password} = req.body;
+		const existedUser = await User.findOne({email});
+    
+		if (!existedUser) {
+			console.log('Unexisted user');
+			throw new  BadRequestError('Invalid credential provided');
+		}
+		
+		const passwordMatch = await Password.compare(existedUser.password, password);
+		
+		if (!passwordMatch) {
+			throw new  BadRequestError('Invalid credential provided');
+		}
+	// Generate JWT
+	const existedUserJwt = jwt.sign({
+		id: existedUser.id,
+		email: existedUser.email
+	}, process.env.JWT_KEY!);
+
+	// Store it on session object
+	req. session = {
+		jwt: existedUserJwt
+	};
+
+	res.status(200).send(existedUser);
+
+	}
+);
 
 export {router as signinRouter};
 
